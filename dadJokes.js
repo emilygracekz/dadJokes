c8eff5b48c8a904e80f6d848a0bc812ef32e1191
Emily <57361805+emilygracekz@users.noreply.github.com>
2020-02-24 22:46:29 +0200

    Add files via upload

---
+++ dadJokes.js
@@ -0,0 +1,40 @@
+const jokeButton = document.querySelector(".getJoke");
+const jokeHolder = document.querySelector(".joke p");
+
+const buttonText = [
+    'ugh.',
+    '🤦🏼‍♀️',
+    'omg dad.',
+    'you are the worst.',
+    'seriously.',
+    'stop it.',
+    'please stop.',
+    'that was the worst one.',
+];
+
+async function fetchJoke() {
+    const response = await fetch('https://icanhazdadjoke.com', {
+        headers: {
+            Accept: 'application/json',
+        },
+    });
+    const data = await response.json()
+    return data;
+}
+
+function randomItemFromArray(arr, not) {
+    const item = arr[Math.floor(Math.random() * arr.length)];
+    if(item === not) {
+        randomItemFromArray(arr, not);
+    }
+    return item;
+}
+
+async function handleClick() {
+const { joke } = await fetchJoke();
+jokeHolder.textContent = joke;
+jokeButton.textContent = randomItemFromArray(buttonText, jokeButton.textContent);
+}
+
+jokeButton.addEventListener('click', handleClick);
+
